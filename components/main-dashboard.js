3bee1bbdd0b90ed138b4933b56b648a86f2e8f7a
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 17:39:26 -0500

    add: basic dashboard

--- components/main-dashboard.js
+++ components/main-dashboard.js
@@ -1,21 +1,70 @@
-// medicaments
-// history
-// reminders
-// ...
+import { Chart, ArcElement } from "chart.js";
+import { Container, Card, Row, Col } from "react-bootstrap";
+import { Doughnut } from "react-chartjs-2";
+Chart.register(ArcElement);
 
-import { Tab, Tabs } from "react-bootstrap";
+const DummyChart = () => (
+  <Doughnut
+    data={{
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255,99,132,1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    }}
+  />
+);
 
-// show tabs with information for each field: medicaments, history, reminders, ...
+// center title text
+const DummyCard = ({ title, children }) => (
+  <Col sm={12} md={4} lg={3} className="mt-3">
+    <Card className="h-100">
+      <Card.Body>
+        <Card.Title className="text-center">{title}</Card.Title>
+        {children}
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+const Summary = () => (
+  <p>summary</p>
+);
+
 export default function MainDashboard() {
-  const tabs = ["Medicaments", "Med History", "Reminders"];
-
+  // each name gets its own card with a dummy chart
   return (
-    <Tabs defaultActiveKey="Medicaments" id="uncontrolled-tab-example">
-      {tabs.map((tab, index) => (
-        <Tab eventKey={tab} title={tab} key={index}>
-          {tab}
-        </Tab>
-      ))}
-    </Tabs>
+    <Container>
+      <Row>
+        <DummyCard title="Summary">
+          <Summary />
+        </DummyCard>
+
+        {["Pulse", "Pressure", "Sugar"].map((name) => (
+          <DummyCard key={name} title={name}>
+            <DummyChart />
+          </DummyCard>
+        ))}
+      </Row>
+    </Container>
   );
 }
