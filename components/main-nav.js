254ee8c6920cd3c55cafacbdb7625878661170ef
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 12:17:00 -0500

    change: change nav brand

--- components/main-nav.js
+++ components/main-nav.js
@@ -9,11 +9,11 @@ import Link from "next/link";
 export default function MainNav() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Health Intelligence</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Link href="#home" passHref>
+          <Link href="/" passHref>
             <Nav.Link>Home</Nav.Link>
           </Link>
           <Link href="#link" passHref>
