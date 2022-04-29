54f3701dd03d5933352fe91189a6c40dbbcbd36b
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 11:48:59 -0500

    add: react bootstrap; navbar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-import Head from 'next/head'
-import Button from 'react-bootstrap/Button'
+import Head from "next/head";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,9 +15,35 @@ export default function Home() {
         <meta name="description" content="Hackathon Web Prototype" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hackathon</h1>
-      <p>hello, there</p>
-      <Button variant="primary">click me</Button>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link href="#home" passHref>
+              <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link href="#link" passHref>
+              <Nav.Link>Link</Nav.Link>
+            </Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
-  )
+  );
 }
