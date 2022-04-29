e5c378bdd198facdda1926f09887c35132534abe
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 17:52:57 -0500

    add: generate chard data randomly

--- components/main-dashboard.js
+++ components/main-dashboard.js
@@ -3,36 +3,32 @@ import { Badge, Container, Card, Row, Col, ListGroup } from "react-bootstrap";
 import { Doughnut } from "react-chartjs-2";
 Chart.register(ArcElement);
 
-const DummyChart = () => (
-  <Doughnut
-    data={{
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
-        {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
+// generate random data for a two part doughnut chart
+const DummyChart = () => {
+  const data = {
+    labels: ["Less", "More"],
+    datasets: [
+      {
+        data: [
+          Math.floor(Math.random() * 100),
+          Math.floor(Math.random() * 100),
+        ],
+        backgroundColor: ["#FF6384", "#36A2EB"],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB"],
+      },
+    ],
+  };
+  return (
+    <Doughnut
+      data={data}
+      options={{
+        legend: {
+          position: "right",
         },
-      ],
-    }}
-  />
-);
+      }}
+    />
+  );
+};
 
 // center title text, right aligned badge
 const DummyCard = ({ title, children, bigger, badge }) => (
