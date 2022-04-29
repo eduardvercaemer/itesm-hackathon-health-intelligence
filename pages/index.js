5d87bf85c0b723382a7c633d111edd227c73af3f
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 12:09:57 -0500

    add: basic data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import MainNav from "../components/main-nav";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/user")
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  let content;
+  if (loading) {
+    content = <p>Loading...</p>;
+  } else {
+    content = (
+      <ul>
+        {data.map((user) => (
+          <li key={user.username}>{user.username}</li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <div>
       <Head>
@@ -10,6 +36,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainNav />
+      <main>{content}</main>
     </div>
   );
 }
