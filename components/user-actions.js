f8411d5158ead39a7ba0ebe108a53b9bfeac3e30
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 21:04:06 -0500

    add: better user suggestion info

--- components/user-actions.js
+++ components/user-actions.js
@@ -1,13 +1,31 @@
-import { ListGroup, Badge, ListGroupItem } from "react-bootstrap";
+import { Button, ListGroup, Badge, Accordion, Alert } from "react-bootstrap";
 
 // item with text, optional badge, and checkbox
-const SuggestedAction = ({ text, badge }) => (
+const SuggestedAction = ({ text, badge, index, description }) => (
   <ListGroup.Item>
     <input type="checkbox" /> {text}
     {badge && (
-      <Badge pill variant="primary" className="float-right">
-        {badge}
-      </Badge>
+      <>
+        {" "}
+        <Badge pill variant="primary">
+          {badge}
+        </Badge>
+      </>
+    )}
+    {description && (
+      <>
+        <Accordion.Toggle
+          className="float-right"
+          as={Button}
+          variant="link"
+          eventKey={index}
+        >
+          Details
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey={index}>
+          <Alert className="mt-3" variant="info">{description}</Alert>
+        </Accordion.Collapse>
+      </>
     )}
   </ListGroup.Item>
 );
@@ -22,12 +40,19 @@ const PastAction = ({ text }) => (
 export default function UserActions() {
   return (
     <>
-      <ListGroup variant="flush">
-        <SuggestedAction text="Eat a healthy meal" />
-        <SuggestedAction text="Drink a glass of water" />
-        <SuggestedAction text="Take a walk" />
-        <SuggestedAction text="Exercise" badge="!!!" />
-      </ListGroup>
+      <Accordion>
+        <ListGroup variant="flush">
+          <SuggestedAction
+            text="Eat a healthy meal"
+            index="0"
+            description="This is likely to improve sugar levels in people with similar conditions"
+            badge="1"
+          />
+          <SuggestedAction text="Drink a glass of water" index="1" />
+          <SuggestedAction text="Take a walk" index="2" />
+          <SuggestedAction text="Exercise" badge="!!!" index="3" />
+        </ListGroup>
+      </Accordion>
       <ListGroup variant="flush">
         <PastAction text="Eat a healthy meal" />
       </ListGroup>
