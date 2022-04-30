5c2e563234091f8e5ee7ed95431a6b561ae3cc0a
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-30 04:06:17 -0500

    add: doctor calendar

--- pages/doctor.js
+++ pages/doctor.js
@@ -1,25 +1,44 @@
-import { ListGroup, Button } from "react-bootstrap";
+import { ListGroup, Button, Container, Col, Row } from "react-bootstrap";
+import Calendar from "react-calendar";
 // import a cellphone icon
 import { FaPhone } from "react-icons/fa";
+import "react-calendar/dist/Calendar.css";
 
 // a bootstrap list group with fields for doctor information
 // each item has a title and content inline
 const DoctorInfo = () => (
-  <ListGroup variant="flush">
-    <ListGroup.Item>
-      <h5>Dr. John Doe</h5>
-    </ListGroup.Item>
-    <ListGroup.Item>
-      <h5>Office: xxxxxxxxxxxx</h5>
-    </ListGroup.Item>
-    <ListGroup.Item>
-      <h5 style={{ display: "inline" }}>Cellphone: aa bbb bbb ccc</h5>
-      <Button variant="primary" className="float-right">
-        <FaPhone />
-      </Button>
-    </ListGroup.Item>
-    <ListGroup.Item>Citas</ListGroup.Item>
-  </ListGroup>
+  <>
+    <Container>
+      <Row>
+        <Col xs={12} md={8} className="mx-auto">
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h5>Dr. John Doe</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h5>Office: xxxxxxxxxxxx</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h5 style={{ display: "inline" }}>Cellphone: aa bbb bbb ccc</h5>
+              <Button variant="primary" className="float-right">
+                <FaPhone />
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <h3>Availability</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={8} className="mx-auto">
+          <Calendar className="mx-auto" />
+        </Col>
+      </Row>
+    </Container>
+  </>
 );
 
 export default function Doctor() {
