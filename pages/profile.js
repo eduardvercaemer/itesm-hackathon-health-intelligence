efe3246821ef02e92cb7ff7695aeac7d0acee1c0
Vercaemer Eduard <vercaemereduard@gmail.com>
2022-04-29 21:57:03 -0500

    improve: user profile

--- pages/profile.js
+++ pages/profile.js
@@ -3,28 +3,41 @@ import { Container, Image, Row, Col, Card } from "react-bootstrap";
 // dummy profile picture component from internet
 const ProfilePic = () => <Image src="https://picsum.photos/50" roundedCircle />;
 
-// in card header, float profile pic and name to the left
+// profile card component, shows name, date of birth, list of chronic diseases
+// list of medications, and list of allergies
 export default function Profile() {
   return (
     <Container>
       <Row className="mt-3">
         <Col>
           <Card>
-            <Card.Header>
+            <Card.Body>
               <Row>
-                <Col className="float-left">
-                  <ProfilePic />
-                </Col>
-                <Col className="float-left">
-                  <h1>John Doe</h1>
+                <Col>
+                  <Card.Title>
+                    <h1>John Doe</h1>
+                  </Card.Title>
+                  <Card.Text>
+                    <h5>Date of Birth:</h5>
+                    <p>January 1, 1970</p>
+                  </Card.Text>
+                  <Card.Text>
+                    <h5>Chronic Diseases:</h5>
+                    <p>
+                      Diabetes, High Blood Pressure, Asthma, High Cholesterol,
+                      Cancer
+                    </p>
+                  </Card.Text>
+                  <Card.Text>
+                    <h5>Medications:</h5>
+                    <p>Aspirin, Ibuprofen, Advil</p>
+                  </Card.Text>
+                  <Card.Text>
+                    <h5>Allergies:</h5>
+                    <p>Peanuts, Eggs, Milk</p>
+                  </Card.Text>
                 </Col>
               </Row>
-            </Card.Header>
-            <Card.Body>
-              <Card.Text>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Card.Text>
             </Card.Body>
           </Card>
         </Col>
